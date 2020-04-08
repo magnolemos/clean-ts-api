6db9c39a3fb185cfea1ecb7d6bf14453b21d6dac
Magno Lemos <magnolcardoso@gmail.com>
2020-04-07 21:57:01 -0300

    refactor: simplify code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,11 +4,11 @@ import { badRequest } from '../helpers/httpHelper'
 
 export class SignUpController {
   handle (httpResquet: HttpRequest): HttpResponse {
-    if (!httpResquet.body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
-    if (!httpResquet.body.email) {
-      return badRequest(new MissingParamError('email'))
+    const requiredFields = ['name', 'email']
+    for (const field of requiredFields) {
+      if (!httpResquet.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
   }
 }
