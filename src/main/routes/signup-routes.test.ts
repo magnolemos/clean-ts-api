d313faf3142d5909cd7e52208a53091b00512aea
Magno Lemos <magnolcardoso@gmail.com>
2020-04-30 17:07:16 -0300

    test: ensure SignUp route returns an account on success

---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,15 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Signup Routes', () => {
+  test('Should return an account on success', async () => {
+    await request(app)
+      .post('/api/signup').send({
+        name: 'Magno',
+        email: 'magnolcardoso@gmail.com',
+        password: '123',
+        passwordConfirmation: '123'
+      })
+      .expect(200)
+  })
+})
