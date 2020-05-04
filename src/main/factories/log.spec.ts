3836a7c7f4170b5688792209d966919d985e8daf
Magno Lemos <magnolcardoso@gmail.com>
2020-05-04 15:33:47 -0300

    test: ensure LogControllerDecorator return the same results of the controller

--- src/main/factories/log.spec.ts
+++ src/main/factories/log.spec.ts
@@ -45,4 +45,23 @@ describe('LogControllerDecorator', () => {
     await sut.handle(httpRequest)
     expect(handleSpy).toBeCalledWith(httpRequest)
   })
+
+  test('Should return teh same results os the controller ', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {
+        name: 'Magno'
+      }
+    })
+  })
 })
